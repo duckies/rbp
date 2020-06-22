ffa515058d3bd16e4285e10eb30545c9ac7aa273
John Gilbert <gilbert.johnt@gmail.com>
2020-06-22 16:53:22 -0400

    refactor: fork EntityManager in raid queue

--- src/raid/raid.queue.ts
+++ src/raid/raid.queue.ts
@@ -1,82 +1,81 @@
 import { OnQueueCompleted, OnQueueError, OnQueueFailed, Process, Processor } from '@nestjs/bull';
-import { BadRequestException, Logger } from '@nestjs/common';
+import { BadRequestException, Logger, Injectable } from '@nestjs/common';
 import { Job } from 'bull';
+import { EntityManager, MikroORM } from 'mikro-orm';
 import { RaiderIOService } from '../raiderIO/raiderIO.service';
 import { Raid } from './raid.entity';
-import { RaidService } from './raid.service';
 
+@Injectable()
 @Processor('raid')
 export class RaidQueue {
   private readonly logger: Logger = new Logger(RaidQueue.name);
+  private readonly em: EntityManager;
 
-  constructor(private readonly raidService: RaidService, private readonly raiderIOService: RaiderIOService) {}
+  constructor(private readonly orm: MikroORM, private readonly raiderIOService: RaiderIOService) {
+    this.em = orm.em.fork();
+  }
 
   @Process({ name: 'updateRaids' })
-  private async updateRaids(job: Job<number>): Promise<Raid[]> {
-    const raiderIOData = await this.raiderIOService.getGuildRaiderIO();
-    const raids = await this.raidService.findAllBySlugs(Object.keys(raiderIOData.raid_rankings));
-    const promises = [];
-    let progress = 0;
+  private async updateRaids() {
+    const data = await this.raiderIOService.getGuildRaiderIO();
 
-    if (!raiderIOData.raid_progression || !raiderIOData.raid_rankings) {
-      throw new BadRequestException({
-        message: 'No data available for RaiderIO guild update.',
-        raiderIOData,
-      });
+    if (!data.raid_progression || !data.raid_rankings) {
+      throw new BadRequestException('No data available for RaiderIO guild update.');
     }
 
-    for (const raidSlug of Object.keys(raiderIOData.raid_rankings)) {
-      let raid = raids.find((r) => r.slug === raidSlug);
+    const raids = await this.em.find(Raid, { slug: { $in: Object.keys(data.raid_rankings) } });
+
+    for (const slug of Object.keys(data.raid_rankings)) {
+      let raid = raids.find((r) => r.slug === slug);
 
       if (!raid) {
-        raid = new Raid();
-        raid.slug = raidSlug;
+        raid = new Raid(slug);
+        this.em.persist(raid);
       } else if (raid.locked) {
         continue;
       }
 
-      raid.summary = raiderIOData.raid_progression[raid.slug].summary;
-      raid.bosses = raiderIOData.raid_progression[raid.slug].total_bosses;
-      raid.normal_bosses_killed = raiderIOData.raid_progression[raid.slug].normal_bosses_killed;
-      raid.heroic_bosses_killed = raiderIOData.raid_progression[raid.slug].heroic_bosses_killed;
-      raid.mythic_bosses_killed = raiderIOData.raid_progression[raid.slug].mythic_bosses_killed;
+      raid.summary = data.raid_progression[raid.slug].summary;
+      raid.bosses = data.raid_progression[raid.slug].total_bosses;
+      raid.normal_bosses_killed = data.raid_progression[raid.slug].normal_bosses_killed;
+      raid.heroic_bosses_killed = data.raid_progression[raid.slug].heroic_bosses_killed;
+      raid.mythic_bosses_killed = data.raid_progression[raid.slug].mythic_bosses_killed;
 
-      if (raiderIOData.raid_rankings[raid.slug].mythic.world > 0) {
+      if (data.raid_rankings[raid.slug].mythic.world > 0) {
         raid.difficulty = 'Mythic';
-        raid.world = raiderIOData.raid_rankings[raid.slug].mythic.world;
-        raid.region = raiderIOData.raid_rankings[raid.slug].mythic.region;
-        raid.realm = raiderIOData.raid_rankings[raid.slug].mythic.realm;
+        raid.world = data.raid_rankings[raid.slug].mythic.world;
+        raid.region = data.raid_rankings[raid.slug].mythic.region;
+        raid.realm = data.raid_rankings[raid.slug].mythic.realm;
         raid.progress = raid.mythic_bosses_killed / raid.bosses;
-      } else if (raiderIOData.raid_rankings[raid.slug].heroic.world > 0) {
+      } else if (data.raid_rankings[raid.slug].heroic.world > 0) {
         raid.difficulty = 'Heroic';
-        raid.world = raiderIOData.raid_rankings[raid.slug].heroic.world;
-        raid.region = raiderIOData.raid_rankings[raid.slug].heroic.region;
-        raid.realm = raiderIOData.raid_rankings[raid.slug].heroic.realm;
+        raid.world = data.raid_rankings[raid.slug].heroic.world;
+        raid.region = data.raid_rankings[raid.slug].heroic.region;
+        raid.realm = data.raid_rankings[raid.slug].heroic.realm;
         raid.progress = raid.heroic_bosses_killed / raid.bosses;
       } else {
         raid.difficulty = 'Normal';
-        raid.world = raiderIOData.raid_rankings[raid.slug].normal.world;
-        raid.region = raiderIOData.raid_rankings[raid.slug].normal.region;
-        raid.realm = raiderIOData.raid_rankings[raid.slug].normal.realm;
+        raid.world = data.raid_rankings[raid.slug].normal.world;
+        raid.region = data.raid_rankings[raid.slug].normal.region;
+        raid.realm = data.raid_rankings[raid.slug].normal.realm;
         raid.progress = raid.normal_bosses_killed / raid.bosses;
       }
-
-      promises.push(this.raidService.upsert(raid));
     }
 
-    promises.forEach((p) => p.then(() => job.progress(progress++ / promises.length)));
+    await this.em.flush();
 
-    return Promise.all(promises);
+    return raids;
   }
 
   @OnQueueError()
-  private onError(_job: Job<number>, error: Error): void {
+  private onError(error: Error): void {
     this.logger.error(error);
   }
 
   @OnQueueCompleted()
   private onCompleted(job: Job<number>, result: Raid[]): void {
-    this.logger.log(`Roster update completed for ${result.length} raids.`);
+    this.logger.log(`Update completed for ${result.length} raids.`);
+    this.em.clear();
   }
 
   @OnQueueFailed()
--- src/raid/raid.scheduler.ts
+++ src/raid/raid.scheduler.ts
@@ -1,12 +1,10 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { Queue } from 'bull';
 import { InjectQueue } from '@nestjs/bull';
+import { Injectable } from '@nestjs/common';
+import { Queue } from 'bull';
 import { Cron, NestSchedule } from 'nest-schedule';
 
 @Injectable()
 export class RaidScheduler extends NestSchedule {
-  private readonly logger: Logger = new Logger(RaidScheduler.name);
-
   constructor(@InjectQueue('raid') private readonly queue: Queue) {
     super();
   }
