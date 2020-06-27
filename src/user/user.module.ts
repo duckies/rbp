1c405582a7afc3c8f54c6ed979b93c6314070ed2
John Gilbert <gilbert.johnt@gmail.com>
2020-06-27 19:22:35 -0400

    refactor: remove unused user queue for now

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,16 +1,13 @@
-import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { MikroOrmModule } from 'nestjs-mikro-orm';
 import { UserController } from './user.controller';
 import { User } from './user.entity';
-import { UserQueue } from './user.queue';
-import { UserScheduler } from './user.scheduler';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [MikroOrmModule.forFeature({ entities: [User] }), BullModule.registerQueue({ name: 'user' })],
+  imports: [MikroOrmModule.forFeature({ entities: [User] })],
   controllers: [UserController],
-  providers: [UserService, UserQueue, UserScheduler],
+  providers: [UserService],
   exports: [UserService],
 })
 export class UserModule {}
