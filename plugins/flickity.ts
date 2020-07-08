6e1f0f2daea05089381c845ba7ab4756b72c9201
John Gilbert <gilbert.johnt@gmail.com>
2020-07-08 19:53:13 -0400

    style: add todo commit to remove flickity

--- plugins/flickity.ts
+++ plugins/flickity.ts
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+// TODO: Replace this library or the slider altogether, bad UX all around.
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
 import Flickity from 'vue-flickity'
